cd4f3dd147edee8ffcd7202c00b18bd2a99be800
MassivePYR <jeovah02@gmail.com>
2022-07-01 01:10:10 -0300

    add

--- lista 1c/7-intIP-listac.c
+++ lista 1c/7-intIP-listac.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 
-int main()
-{
-    double p = 8, l = 5, tf = 1, matricula;
+int main(){
     double p1, p2, p3, p4, p5, p6, p7, p8, l1, l2, l3, l4, l5, TF, Mp, Ml, Nf, Pa, fT;
-    scanf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",&matricula,&p1,&p2,&p3,&p4,&p5,&p6,&p7,&p8,&l1,&l2,&l3,&l4,&l5,&TF,&Pa);
-    Mp = (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8) / 8;
-    Ml = (l1 + l2 + l3 + l4 + l5) / 5;
-    fT=128-Pa;
-    Nf = ((Mp * 0.7) + (Ml * 0.15) + (TF * 0.15));
-    printf("Matricula: %.2lf, Nota Final: %.2lf, Situacao Final: ",matricula,Nf);
-    if(Nf >= 6)
+    int matricula = 0;
+    do
     {
-        if(fT <= 32)
+        scanf("%d", &matricula);
+        scanf("%lf %lf %lf %lf %lf %lf %lf %lf", &p1, &p2, &p3, &p4, &p5, &p6, &p7, &p8);
+        scanf("%lf %lf %lf %lf %lf %lf %lf", &l1, &l2, &l3, &l4, &l5, &TF, &Pa);
+        Mp = (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8) / 8;
+        Ml = (l1 + l2 + l3 + l4 + l5) / 5;
+        fT = 128 - Pa;
+        Nf = ((Mp * 0.7) + (Ml * 0.15) + (TF * 0.15));
+        if (matricula != -1)
         {
-            printf("APROVADO\n");
+            printf("Matricula: %d, Nota Final: %.2lf, Situacao Final: ", matricula, Nf);
+            if (Nf >= 6)
+            {
+                if (fT <= 32)
+                {
+                    printf("APROVADO\n");
+                }
+                else
+                {
+                    printf("REPROVADO POR FREQUENCIA\n");
+                }
+            }
+            else
+            {
+                if (fT <= 32)
+                {
+                    printf("REPROVADO POR NOTA\n");
+                }
+                else
+                {
+                    printf("REPROVADO POR NOTA E POR FREQUENCIA\n");
+                }
+            }
         }
-        else
-        {
-            printf("REPROVADO POR FREQUENCIA\n");
-        }
-    }
-    else
-    {
-     if(fT<=32){   printf("REPROVADO POR NOTA\n");
-     }
-     else{
-         printf("REPROVADO POR NOTA E POR FREQUENCIA\n");
-     }
-    }
+    } while (matricula != -1);
+
+    return 0;
 }
