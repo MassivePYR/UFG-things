d029c0f366cf1c0c049cfc08e0b23a8e7a1c4d6d
MassivePYR <jeovah02@gmail.com>
2022-07-23 19:14:42 -0300

    att

--- Lista 2/15.c
+++ Lista 2/15.c
@@ -1,15 +1,30 @@
 #include<stdio.h>
+#include<math.h>
 
 int main(){
-    int n,f,vet[f],i,j,aux;
-    scanf("%d",&n);
-    if(n>=1 && n<=10){
-        scanf("%d",&f);
-        if(f>=2 && f<=1000){
-            for(i=0;i>f;i++){
-                scanf("%d",i);
+int n,f,i,j,dis,cont=0,mdis=10000;
+scanf("%d",&n);
+while(n!=0){
+    cont=0;
+    mdis=10000;
+    scanf("%d",&f);
+    int vet[f];
+        for(i=0;i<f;i++){
+            scanf("%d", &vet[i]);
+        }
+    for(i=0;i<f;i++){
+        for(j=i+1;j<f;j++){
+            dis=abs(vet[i]-vet[j]);
+            if(dis<mdis||mdis==0){
+                mdis=dis;
             }
+        } 
+        cont+=i;
+    }
+    printf("%d %d\n", dis, cont);
+    n--;
     }
 }
-}
-
+/*(n-1*n/2);
+#include<string.h>
+strcpy*/
---
+++ outros codes/3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+int i;
+int vl(char x){
+
+    if((int)x<=122&&(int)x>=97){
+        return 1;
+    }
+    else if ((int)x<=90&&(int)x>=65){
+        return 1;
+    }
+    else return 0;
+}
+int vv(char x){
+
+    if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'){
+        return 1;
+    }
+    else if(x=='A'||x=='E'||x=='I'||x=='O'||x=='U'){
+        return 1;
+    }
+    else return 0;
+}
+
+
+int main (){
+
+    int n;
+    int letras, vogais, consoantes;
+    char S[10001];
+
+    scanf("%d", &n);
+    getchar();
+
+    while(n--){
+        letras = 0;
+        vogais = 0;
+        scanf("%[^\n]", S);
+        getchar();
+        for(i=0; S[i]!='\0'; i++){
+            letras += vl(S[i]);
+            vogais += vv(S[i]);
+        }        
+        consoantes = letras-vogais;
+        printf("Letras = %d\nVogais = %d\nConsoantes = %d\n", letras, vogais, consoantes);
+    
+    }
+}
