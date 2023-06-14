fb4ed873e9a340ae10b612e177b06f9220b61673
MassivePYR <jeovah02@gmail.com>
2023-06-14 18:34:03 -0300

    att

--- java/ListaDeHeranca/Ex1/Administrador.java
+++ java/ListaDeHeranca/Ex1/Administrador.java
@@ -1,16 +1,18 @@
 package ListaDeHeranca.Ex1;
 
-public class Administrador extends Funcionario{
+public class Administrador extends Funcionario {
     private Double salario;
 
     public Administrador(String nome, String sobrenome, Double salario) {
         super(nome, sobrenome);
         this.salario = salario;
     }
+
     public void setSalario(Double salario) {
-         this.salario = salario;
+        this.salario = salario;
     }
+
     public String getSalario() {
-         return "Salário: " + this.salario;
+        return "Salário: " + this.salario;
     }
 }
--- java/ListaDeHeranca/Ex1/Funcionario.java
+++ java/ListaDeHeranca/Ex1/Funcionario.java
@@ -1,29 +1,35 @@
 package ListaDeHeranca.Ex1;
 
 public class Funcionario {
-        private String nome;
-        private String sobrenome;
+     private String nome;
+     private String sobrenome;
 
-        public Funcionario(String nome, String sobrenome) {
-            this.nome = nome;
-            this.sobrenome = sobrenome;
-        }
-        public void setNome(String nome) {
-             this.nome = nome;
-        }
-        public void setSobrenome(String sobrenome) {
-             this.sobrenome = sobrenome;
-        }
-        public String getNome() {
-             return this.nome;
-        }
-        public String getSobrenome() {
-             return this.sobrenome;
-        }
-        public String toString() {
-            return "Nome: " + this.nome + "\nSobrenome: " + this.sobrenome;
-        }
-        public String getSalario() {
-            return this.getSalario();
-        }
+     public Funcionario(String nome, String sobrenome) {
+          this.nome = nome;
+          this.sobrenome = sobrenome;
+     }
+
+     public void setNome(String nome) {
+          this.nome = nome;
+     }
+
+     public void setSobrenome(String sobrenome) {
+          this.sobrenome = sobrenome;
+     }
+
+     public String getNome() {
+          return this.nome;
+     }
+
+     public String getSobrenome() {
+          return this.sobrenome;
+     }
+
+     public String toString() {
+          return "Nome: " + this.nome + "\nSobrenome: " + this.sobrenome;
+     }
+
+     public String getSalario() {
+          return this.getSalario();
+     }
 }
